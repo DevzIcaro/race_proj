e2acc9c3b7e2a143150ecf29d57b211fc533756c
DevzIcaro <contato-Devicaro@hotmail.com>
2026-07-03 22:52:07 -0300

    iniivo modulo de inscricoes

---
+++ backend/src/modules/inscricoes/inscricoes.repository.js
@@ -0,0 +1,23 @@
+const prisma = require("../../shared/prisma")
+
+function list(){
+    return prisma.inscricoes.findMany();
+}
+
+function getById(id){
+    return prisma.inscricoes.findUnique({where: {id}});
+}
+
+function create(data){
+    return prisma.inscricoes.create({data});
+}
+
+function update(id, data){
+    return prisma.inscricoes.update({where: {id}, data } );
+}
+
+function remove(id){
+    return prisma.inscricoes.remove(id);
+}
+
+module.exports = {list, getById, create, update, remove}
